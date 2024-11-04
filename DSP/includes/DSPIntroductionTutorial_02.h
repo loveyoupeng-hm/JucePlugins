2d22ccfa9e5b59eada0e5623ce6fc924a2394e89
loveyoupeng <loveyoupeng@gmail.com>
2024-11-04 23:42:31 +0800

    header

--- DSP/includes/DSPIntroductionTutorial_02.h
+++ DSP/includes/DSPIntroductionTutorial_02.h
@@ -47,10 +47,10 @@
 
 #pragma once
 
-#include <math.h>
-#include <float.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_dsp/juce_dsp.h>
+#include "JucePluginDefines.h"
 
 //==============================================================================
 template <typename Type>
