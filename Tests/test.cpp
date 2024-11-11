22e3fe6e5ee798877426f358bd3d0ab86cb47776
loveyoupeng <loveyoupeng@gmail.com>
2024-11-11 22:51:36 +0800

    test case oscillator

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,13 +1,19 @@
 #include <gtest/gtest.h>
 #include "PluginProcessor.h"
+#include <juce_dsp/juce_dsp.h>
 
 namespace test_plugins
 {
 
-    TEST(Simple, Foo)
+    TEST(Juce, Oscillator)
     {
-        AudioPluginAudioProcessor processor{};
-        EXPECT_TRUE(true);
+        juce::dsp::Oscillator<float> osc;
+        EXPECT_FALSE(osc.isInitialised());
+        osc.initialise([](float x)
+                       { return x; });
+        EXPECT_TRUE(osc.isInitialised());
+        osc.setFrequency(1);
+        EXPECT_EQ(1.0, osc.getFrequency());
     }
-    
+
 } // namespace test_plugins
